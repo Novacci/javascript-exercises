aa28323f12b68ad79c9b95f1f3be147a9d624b8e
Wojciech Nowak <wojtekn00@gmail.com>
2022-10-27 15:11:20 +0200

    repeatString task nr2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+// const repeatString = function (display, number) {
+//   for (i = 0; i < number; i++) {}
+//   let result = display.repeat(number);
+//   console.log(result);
+//   return result;
+// };
 
+const repeatString = function (display, number) {
+  let result = '';
+  if (number < 0) {
+    return 'ERROR';
+  }
+  for (i = 0; i < number; i++) {
+    result += display;
+  }
+  return result;
 };
 
 // Do not edit below this line
