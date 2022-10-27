785daa25886ef56bc59de097e57eaf6bf50e01d5
Wojciech Nowak <wojtekn00@gmail.com>
2022-10-27 16:39:35 +0200

    task 3 done - reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+// const reverseString = function (word) {
+//   if ((word = '')) {
+//     word.split('').reverse().join('');
+//   }
+// };
 
+const reverseString = function (word) {
+  return word.split('').reverse().join('');
 };
-
 // Do not edit below this line
 module.exports = reverseString;
