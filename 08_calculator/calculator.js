7320a710195c588ca790cd1b1276ab713d2aa774
Wojciech Nowak <wojtekn00@gmail.com>
2022-11-01 22:50:00 +0100

    task halfly done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let sub = a - b;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  let result = 0;
+  for (let i = 0; i < a.length; i++) {
+    result += a[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  let result = a[0];
+  for (let i = 1; i < a.length; i++) {
+    result = result * a[i];
+  }
+  return result;
 };
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
