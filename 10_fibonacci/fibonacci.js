7b6a06da0b4da652fe9e4e0b44ec7f0ff0ad7067
Wojciech Nowak <wojtekn00@gmail.com>
2022-11-05 15:36:46 +0100

    halfly done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num === 0) {
+    return 0;
+  } else if (num === 1 || num === 2) {
+    return 1;
+  } else if (num > 2) {
+    return (num = num - 1 + (num - 2));
+  }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+```javascript
+fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
+fibonacci(6) // returns 8
+```;
+// <!-- 1 1 2 3 5 8 -->
+// fibonacci(2) // return the (1,1)
+// fibonacci(3) // return the (1,1,2)
